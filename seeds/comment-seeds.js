166156930b9b44f1b12290d91bbe022578a618e9
lconnell1000 <lconnell1000@gmail.com>
2022-03-22 20:13:51 +1030

    fixed some of the seeds data

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -7,12 +7,12 @@ const commentData = [
         comment_text: "HA, YOU WISH!"
     },
     {
-        user_id: 2,
+        user_id: 3,
         post_id: 3,
         comment_text: "YOU REALLY THINK SO!?"
     },
     {
-        user_id: 3,
+        user_id: 2,
         post_id: 1,
         comment_text: "DEBATABLE!!"
     },
