29d4ac3e7da9604f4ab5f8a6827d982415ab9466
lconnell1000 <lconnell1000@gmail.com>
2022-03-22 20:06:40 +1030

    comment seeds finished

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,24 @@
+const { Comment } = require('../models');
+
+const commentData = [
+    {
+        user_id: 1,
+        post_id: 2,
+        comment_text: "HA, YOU WISH!"
+    },
+    {
+        user_id: 2,
+        post_id: 3,
+        comment_text: "YOU REALLY THINK SO!?"
+    },
+    {
+        user_id: 3,
+        post_id: 1,
+        comment_text: "DEBATABLE!!"
+    },
+   
+]
+
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
